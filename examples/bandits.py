170be01c76e87749d9a5166215ced867da4082ea
Chris Cooper <chris@cooper.info>
2022-08-10 23:19:32 +1000

    Implement thompson sampling for bernoulli bandits

--- examples/bandits.py
+++ examples/bandits.py
@@ -1,15 +1,21 @@
+from importlib.metadata import distribution
 import random
-
+import numpy as np
+from scipy.stats import beta
 
 def main():
     print("bandits")
 
-    # Initialise the gambling machines
-    num_machines = 3
-    machines = [GamblingMachine() for i in range(num_machines)]
+    # Initialise the gambling machines and the RL agent. 
+    num_machines = 50
+    machines = [GamblingMachine() for _ in range(num_machines)]
+    agent = Agent(num_machines)
+
     print("{} machines to select from.".format(num_machines))
 
-    loop(machines)
+    # loop_human(machines)
+    num_iterations = num_machines * 10
+    loop_agent(machines, agent, num_iterations)
 
     best_payout = 0.0
     best_index = 0
@@ -19,8 +25,12 @@ def main():
             best_index = i
 
     print("Best machine is number {}, which pays out {}.".format(best_index+1, best_payout))
+    print("The agent selected machine number {}".format(agent.best_machine()+1))
+    print("Machines: ")
+    for i, machine in enumerate(machines):
+        print("    {}, payout = {}".format(i, machine.payout))
 
-def loop(machines):
+def loop_human(machines):
     if len(machines) < 1:
         return
 
@@ -43,6 +53,12 @@ def loop(machines):
         bet = 1.0
         result = machines[action-1].gamble()
         print("Bet {}, received {}.".format(bet, result))
+
+def loop_agent(machines, agent, num_iterations = 100):
+    for _ in range(num_iterations):
+        machine_inx = agent.action()
+        success = bool(machines[machine_inx].gamble())
+        agent.update(success, machine_inx)
         
 
 class GamblingMachine:
@@ -51,10 +67,47 @@ class GamblingMachine:
 
     def gamble(self, amount=1.0):
         trial = random.random()
-        if trial > self.payout:
+        if trial > (1 - self.payout):
             return amount * 2.0
         else:
             return 0.0
+
+class BetaDistribution:
+    def __init__(self):
+        # alpha and beta are the shape parameters of the beta distribution .
+        # Alph counts the sucesses. Beta counts the failures. 
+        self.alpha = 1 
+        self.beta = 1 
+
+    def sample(self) -> float:
+        return beta.rvs(self.alpha, self.beta) # Does this do what I think it does?
+
+
+class Agent:
+    """
+    Agent uses thompson sampling to determine the best gambling machine.
+    action : returns the index of the gambling machine to play. Samples the 
+    beta distribution of the machines and takes the greedy result. 
+    """
+    def __init__(self, num_machines):
+        self.information_state = [BetaDistribution() for _ in range(num_machines)]
+
+    def action(self) -> int:
+        samples = [machine.sample() for machine in self.information_state]
+        max_index = samples.index(max(samples))
+        return max_index
+
+    def update(self, success: bool, selected_action: int):
+        if success:
+            self.information_state[selected_action].alpha += 1
+        else:
+            self.information_state[selected_action].beta += 1
+
+    def best_machine(self):
+        expected_returns = [beta.stats(machine.alpha, machine.beta, moments='m') \
+            for machine in self.information_state]
+        best_machine_inx = expected_returns.index(max(expected_returns))
+        return best_machine_inx
 
 if __name__ == "__main__":
     main()
